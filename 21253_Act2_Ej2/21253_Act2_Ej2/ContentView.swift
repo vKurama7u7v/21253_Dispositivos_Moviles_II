46a8255e8bb1204fd7d735851cceec05a337b883
vKurama7u7v <aurelio.marin@iest.edu.mx>
2023-09-06 18:18:45 -0600

    first commit

---
+++ 21253_Act2_Ej2/21253_Act2_Ej2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  21253_Act2_Ej2
+//
+//  Created by Aurelio Marín Bautista on 06/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 21253_Act2_Ej2/21253_Act2_Ej2/_1253_Act2_Ej2App.swift
@@ -0,0 +1,17 @@
+//
+//  _1253_Act2_Ej2App.swift
+//  21253_Act2_Ej2
+//
+//  Created by Aurelio Marín Bautista on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct _1253_Act2_Ej2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
