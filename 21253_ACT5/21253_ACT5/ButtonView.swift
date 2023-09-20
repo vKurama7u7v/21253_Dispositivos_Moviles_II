65e4531bded6f8fdce90311cf5dfde49e9ff9c4b
vKurama7u7v <aurelio.marin@iest.edu.mx>
2023-09-20 17:14:04 -0600

    add ACT 5

---
+++ 21253_ACT5/21253_ACT5/ButtonView.swift
@@ -0,0 +1,44 @@
+//
+//  ButtonView.swift
+//  21253_ACT5
+//
+//  Created by Aurelio Marín Bautista on 20/09/23.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    
+    var text : String
+    var icon : String
+    var color : Color
+    var sizeButton : CGFloat = 60
+    
+    var body: some View {
+        VStack{
+            Image(systemName: icon)
+                .imageScale(.large)
+                .foregroundColor(color)
+                .frame(width: sizeButton, height: sizeButton)
+                .background(Color.white)
+                .cornerRadius(100)
+                .padding(4)
+                .background(Color("bgButtons"))
+                .cornerRadius(100)
+                
+            Text(text)
+                .font(.system(size: 12))
+                .fontWeight(.bold)
+            Spacer()
+        }
+        .padding(.horizontal, 4)
+        .padding(.vertical, 2)
+        .foregroundColor(Color.gray)
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView(text: "name", icon: "envelope", color: Color.gray)
+    }
+}
--- 21253_ACT5/21253_ACT5/ContentView.swift
+++ 21253_ACT5/21253_ACT5/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(){
+            // Información de Usuario
             HStack(){
                 HStack(){
                     Image(systemName: "person")
@@ -18,7 +19,10 @@ struct ContentView: View {
                         .padding(12)
                         .background(Color.white)
                         .cornerRadius(50)
-                    Text("Hola, Aurelio ").foregroundColor(Color.white).fontWeight(.semibold)
+                    Text("Hola, Aurelio ")
+                        .foregroundColor(Color.white)
+                        .fontWeight(.semibold)
+                        .font(.system(size: 18))
                     Image(systemName: "chevron.right")
                         .imageScale(.small)
                         .foregroundColor(Color.white)
@@ -39,6 +43,7 @@ struct ContentView: View {
             }.padding()
             
             ScrollView{
+                // Saldo
                 VStack(){
                     VStack(){
                         HStack(){
@@ -93,22 +98,103 @@ struct ContentView: View {
                 }
                 .background(Color.white)
                 .cornerRadius(10)
+                .padding(.bottom, 8)
                 
-            }.padding()
-            
-            Spacer()
-        }.background(LinearGradient(
+                // Credito
+                VStack(){
+                    HStack(){
+                        Image(systemName: "eye")
+                            .imageScale(.medium)
+                            .foregroundColor(Color.black)
+                        Text("Créditos")
+                            .fontWeight(.bold)
+                            .font(.system(size: 18))
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .imageScale(.small)
+                            .foregroundColor(Color.blue)
+                    }
+                    Text("Consulta tu límite disponibe y usa tu línea de credito como prefieras.")
+                        .font(.system(size: 14))
+                        .padding(.vertical, 1)
+                }
+                .foregroundColor(Color("darkText"))
+                .padding(20)
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.bottom, 8)
+                
+                // Anuncio
+                VStack(){
+                    
+                    HStack(){
+                        Text("Consigue envíos gratis en más productos con meli+")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 11))
+                            .fontWeight(.bold)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .imageScale(.small)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                }
+                .padding(20)
+                .background(LinearGradient(
+                    gradient:
+                        Gradient(
+                            colors:
+                                [
+                                    Color("bgCardPrimary"),
+                                    Color("bgCardSecondary")
+                                ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ))
+                .cornerRadius(10)
+                .padding(.bottom, 8)
+                
+                // Botones Abajo
+                VStack(){
+                    HStack(){
+                        Spacer()
+                        ButtonView(text: "Recargar Celular", icon: "envelope", color: Color.cyan)
+                        ButtonView(text: "Pagar Servicio", icon: "envelope", color: Color.cyan)
+                        ButtonView(text: "Recargar TAG", icon: "envelope", color: Color.cyan)
+                        ButtonView(text: "Cobrar con Point", icon: "envelope", color: Color.blue)
+                        Spacer()
+                        
+                    }
+                    HStack(){
+                        Spacer()
+                        ButtonView(text: "Recargar Transporte", icon: "envelope", color: Color.cyan)
+                        ButtonView(text: "Beneficios", icon: "ticket", color: Color.purple)
+                        ButtonView(text: "Dinero del Extranjero", icon: "globe", color: Color.gray)
+                        ButtonView(text: "Ver Más", icon: "plus", color: Color.gray)
+                        Spacer()
+                        
+                    }
+                }
+                .foregroundColor(Color("darkText"))
+                .padding(20)
+                .background(Color.white)
+                .cornerRadius(10)
+                
+            }.padding(.horizontal, 8)
+        }
+        .background(LinearGradient(
             gradient:
                 Gradient(
                     colors:
                         [
                             Color("bgPrimary"),
-                            Color("bgPrimary"),
+                            Color("bgSecondary"),
                             Color("bgSecondary")
                         ]),
             startPoint: .top,
             endPoint: .bottom
         ))
+        
     }
 }
 
